a17895aec7df2bf9e594442a11b4d429bea99294
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-08 11:39:40 +0100

    advanced dev tools

---
+++ advanced_devtools_start/js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const fruits = ['apple', 'orange', 'banana'];
+    console.table(fruits);
+
+    const person = {
+        name: 'TJ',
+        age: 34,
+    };
+    console.table(person);
+});
