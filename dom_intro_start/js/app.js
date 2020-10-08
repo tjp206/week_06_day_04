1c91366166508b83baeb8e4794ace76ba3341748
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-08 11:33:43 +0100

    intro to dom finished

--- dom_intro_start/js/app.js
+++ dom_intro_start/js/app.js
@@ -1,1 +1,33 @@
+// console.log('App loaded', window);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.querySelector('h1');
+    title.textContent = 'TJ says, Javascript sucks!';
+
+    // const welcomeParagraph = document.querySelector('#welcome-paragraph');
+    // console.log(welcomeParagraph);
+    // console.dir(welcomeParagraph);
+
+    // const redElement = document.querySelector('li.red');
+    // console.log(redElement);
+
+    // const allLiElements = document.querySelectorAll('li');
+    // console.dir(allLiElements);
+
+    // const redListItem = document.querySelector('li.red');
+    // redListItem.textContent = 'RED!!!';
+    // redListItem.classList.add('bold');
+    // console.dir;(redListItem);
+
+    const newListItem = document.createElement('li');
+    newListItem.textContent = 'Purple';
+    newListItem.classList.add('purple');
+
+    const list = document.querySelector('ul');
+    list.appendChild(newListItem);
+    
+
+
+});
+
 
