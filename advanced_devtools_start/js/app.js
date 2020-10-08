006abd3ffc4d74510f234e1d516380a5db806408
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-08 13:05:27 +0100

    dev tools finished

--- advanced_devtools_start/js/app.js
+++ advanced_devtools_start/js/app.js
@@ -1,10 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const fruits = ['apple', 'orange', 'banana'];
-    console.table(fruits);
+    // const fruits = ['apple', 'orange', 'banana'];
+    // console.table(fruits);
 
-    const person = {
-        name: 'TJ',
-        age: 34,
-    };
-    console.table(person);
+    // const person = {
+    //     name: 'TJ',
+    //     age: 34,
+    // };
+    // console.table(person); = 5;
+
+    // debugger
+    // let number1 = 5
+    // number1 += 10;
+    // const number2 = 20 + number1;
+    // const number3 = number2 / 10;
+
+    setTimeout(function (){
+        document.body.classList.add('lightblue');
+    }, 3000);
+
+    
 });
